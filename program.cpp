742da9b0bd70fdd207e84e1bee4730c602d3ceb8
MFarisAdithya <m.farisadithya@gmail.com>
2024-10-06 12:00:49 +0700

    Update program.cpp
    itu else if nya dah gw tambahin

--- program.cpp
+++ program.cpp
@@ -28,3 +28,27 @@ void ask(int bulan) {
     if (jawab == "ada"){
        cout<< "Service yang disarankan : Service berat" << endl;
        cout<< "Total Biaya: 600.000" << endl;
+} 
+    else if (jawab == "tidak") {
+        cout << "Service yang disarankan: ";
+        if (bulan < 2) {
+            cout << "Service ringan" << endl;
+            price = 200000;
+        }
+        else if (bulan <= 4) {
+            cout << "Service berkala" << endl;
+            price = 400000;
+        }
+        else {
+            cout << "Service berat" << endl;
+            price = 600000;
+        }
+        cout << "Harga: Rp." << price << endl;
+        cout << "Total Biaya: Rp." << price << endl;
+    }
+    else {
+        cout << "Input yang dimasukan tidak valid. Tolong masukkan 'ada' atau 'tidak'." << endl;
+    }
+
+    cout << "=============================" << endl;
+}
