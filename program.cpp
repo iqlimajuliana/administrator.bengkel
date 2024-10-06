9a18ef2d3231e44c192702e9290f120bd4f4034a
MZhafiqAindillahSani <m.zhafiqaindillahsani12@gmail.com>
2024-10-06 10:48:57 +0700

    Update program.cpp

--- program.cpp
+++ program.cpp
@@ -1,1 +1,30 @@
-ginii lho
+#include <iosstream>
+using namespace std;
+
+#include <iostream>
+using namespace std;
+
+
+int harga(int bulan) {
+    if(bulan < 2) {
+       retur  200000;
+    }
+    else if (bulan <= 4) {
+         return 400000;
+    }
+    else {
+         return 600000;
+    }
+}
+void ask(int bulan) {
+     string jawab;
+     int price = harga(bulan);
+     cout << "Apakah ada kerusakan di kendaraan anda? (ada / tidak) : ";
+     cin>>jawab;
+
+
+
+
+    if (jawab == "ada"){
+       cout<< "Service yang disarankan : Service berat" << endl;
+       cout<< "Total Biaya: 600.000" << endl;
